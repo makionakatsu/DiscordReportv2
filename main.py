d7b4cc4562449550c4ded6faeb86f667f0f7d406
maki_ <ny.2151102@gmail.com>
2023-05-11 20:44:43 +0900

    2nd commit

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import nextcord as discord
 from nextcord.ext import commands
 import datetime
 import pytz
-import csv
-import re
+import openai
 
+# 指定された時間帯の開始と終了の時間を取得する関数
 def get_start_and_end_times(timezone):
     now = datetime.datetime.now(timezone)
     start_time = now - datetime.timedelta(days=1)
@@ -13,75 +13,72 @@ def get_start_and_end_times(timezone):
     end_time = now.replace(hour=20, minute=30, second=0, microsecond=0)
     return start_time, end_time
 
-def count_emojis(text):
-    emoji_pattern = re.compile(
-        "["
-        u"\U0001F600-\U0001F64F"  # emoticons
-        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           "]+",
-        flags=re.UNICODE,
-    )
-    emojis = emoji_pattern.findall(text)
-    return sum([len(emoji) for emoji in emojis])
-
-def write_log_to_csv(found_messages, target_date):
-    file_name = f"discord_log_{target_date}.csv"
-    with open(file_name, "w", encoding="utf-8", newline="") as file:
-        csv_writer = csv.writer(file)
-        csv_writer.writerow(["Timestamp", "Channel", "Author", "Content", "Message URL", "Emoji Count"])
-
-        for msg in found_messages:
-            jst_created_at = convert_to_jst(msg.created_at)
-            formatted_timestamp = jst_created_at.strftime("%Y-%m-%d %H:%M:%S")
-            message_url = f"https://discord.com/channels/{msg.guild.id}/{msg.channel.id}/{msg.id}"
-            emoji_count = count_emojis(msg.content)
-            csv_writer.writerow([formatted_timestamp, msg.channel.name, str(msg.author), msg.content, message_url, emoji_count])
-
-    return file_name
-
-
+# 日本時間に変換する関数
 def convert_to_jst(dt):
     utc = pytz.utc
     jst = pytz.timezone("Asia/Tokyo")
     return dt.astimezone(jst)
 
-
-async def fetch_logs(guild, start_time, end_time, member=None):
-    found_messages = []
+# Discordからログを取得する非同期関数
+async def fetch_logs(guild, start_time, end_time):
+    found_messages = {}
     for channel in guild.text_channels:
-        if not member:
-            try:
-                async for msg in channel.history(limit=10000):
-                    if start_time <= msg.created_at <= end_time:
-                        found_messages.append(msg)
-            except discord.errors.Forbidden:
-                print(f"Skipping channel {channel.name} due to insufficient permissions.")
-                continue
-        else:
-            try:
-                async for msg in channel.history(limit=10000, user=member):
-                    if start_time <= msg.created_at <= end_time:
-                        found_messages.append(msg)
-            except discord.errors.Forbidden:
-                print(f"Skipping channel {channel.name} due to insufficient permissions.")
-                continue
-
+        try:
+            async for msg in channel.history(limit=10000):
+                if start_time <= msg.created_at <= end_time:
+                    if channel.id not in found_messages:
+                        found_messages[channel.id] = []
+                    # メッセージの内容とIDを辞書に格納します。
+                    message_info = {
+                        "content": msg.content,
+                        "id": msg.id
+                    }
+                    found_messages[channel.id].append(message_info)
+        except discord.errors.Forbidden:
+            print(f"Skipping channel {channel.name} due to insufficient permissions.")
+            continue
     if not found_messages:
         print(f"No messages found for the specified time range.")
-
     return found_messages
 
+# メッセージの要約を生成する関数
+def summarize_text(text):
+    chunks = [text[i:i + 8000] for i in range(0, len(text), 8000)]
+    summarized_chunks = []
+    for chunk in chunks:
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-4",
+                messages=[
+                    {"role": "system", "content": """あなたは、Discordの1日の出来事を受け取り、日本語で感情を込めてわかりやすく、可愛く伝える役割です。
+                     受け取ったテキストを、指定のフォーマットで出力してください。出力フォーマットは、
+                    -チャンネルID
+                    -チャンネルでのメッセージの要約
+                    -【話題ピックアップ】として、チャンネル上のメッセージを５つ程度ピックアップする。
+                    --メッセージごとにメッセージリンクをカッコで括って表示する。:メッセージ（メッセージID ）
+                    --ピックアップは画像やリンクを含むものを優先する。"""},
+                    {"role": "user", "content": f"""フォーマットは以下のとおりです。：
+                     以下のテキストを指定フォーマットの通り出力をしてください
+                     テキスト：{chunk}"""}
+                ],
+                timeout=20  # Increase the timeout value
+            )
+            summarized_chunks.append(response["choices"][0]["message"]["content"])
+        except Exception as e:
+            print(f"Error occurred during API call: {e}")
+            continue
 
-async def send_log_to_channel(guild, log_file_name, channel_id):
+    summarized_text = "\n".join(summarized_chunks)
+    return summarized_text
+
+
+# 要約したメッセージをDiscordチャンネルに送信する
+async def send_summary_to_channel(guild, channel_id, summary):
     channel = guild.get_channel(channel_id)
     if channel is None:
         print(f"Error: Channel with ID {channel_id} not found.")
         return
-
-    with open(log_file_name, "rb") as file:
-        await channel.send(file=discord.File(file, filename=log_file_name))
+    await channel.send(summary)
 
 TOKEN = os.environ["DISCORD_TOKEN"]
 GUILD_ID = int(os.environ["GUILD_ID"])
@@ -93,8 +90,6 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"We have logged in as {bot.user}")
-
     guild = discord.utils.get(bot.guilds, id=GUILD_ID)
     timezone = pytz.timezone("Asia/Tokyo")
     start_time, end_time = get_start_and_end_times(timezone)
@@ -104,11 +99,13 @@ async def on_ready():
         return
 
     found_messages = await fetch_logs(guild, start_time, end_time)
-
+    
     if found_messages:
-        target_date = start_time.strftime("%Y-%m-%d")
-        log_file_name = write_log_to_csv(found_messages, target_date)
-        await send_log_to_channel(guild, log_file_name, CHANNEL_ID)
+        for channel in found_messages.keys():
+            channel_messages = found_messages[channel]
+            messages_text = ' '.join([msg.content for msg in channel_messages])
+            summary = summarize_text(messages_text)
+            await send_summary_to_channel(guild, CHANNEL_ID, summary)
     else:
         print(f"No messages found for the specified time range.")
 
