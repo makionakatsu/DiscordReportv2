b52ba232626205308518e42ba6cc16c52dbcc189
maki_ <ny.2151102@gmail.com>
2023-07-08 20:05:38 +0900

    見直し

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import textwrap
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")  # Discordのトークン
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # OpenAIのAPIキー
 LOG_LEVEL = os.getenv("LOG_LEVEL", "ERROR")  # ログのレベル
+GUILD_ID = os.getenv("GUILD_ID")  # DiscordのサーバーID
 SUMMARY_CHANNEL_NAME = os.getenv("SUMMARY_CHANNEL_NAME", "summary-channel")  # サマリーチャンネル名
 
 # ログ設定
@@ -30,26 +31,33 @@ def get_start_and_end_times(timezone):
     end_time = get_specific_time_on_date(today, 20, 30, 0, 0, timezone)
     return start_time, end_time
 
-async def fetch_logs(guild, start_time, end_time):
-    found_messages = {}
+async def fetch_and_summarize_channel_logs(guild, start_time, end_time):
+    summary_channel = discord.utils.get(guild.text_channels, name=SUMMARY_CHANNEL_NAME)  # specify your summary channel name here
+    if summary_channel is None:
+        logging.error(f"Summary channel not found. Make sure '{SUMMARY_CHANNEL_NAME}' exists.")
+        return
+
     for channel in guild.text_channels:
         try:
             end_time_channel = end_time - CHANNEL_TIME_DELTA
             messages = await channel.history(after=start_time, before=end_time_channel, limit=None).flatten()
             if messages:
-                found_messages[channel.id] = [{
+                messages = [{
                     "content": msg.content,
                     "created_at": msg.created_at,
                     "author": str(msg.author),
                     "attachments": [attachment.url for attachment in msg.attachments],
                     "link": msg.jump_url
                 } for msg in messages if not msg.author.bot]
+
+                contents = ' '.join([message['content'] for message in messages])
+                summarized = summarize_text(contents)
+                logging.info(f"Summary for channel {channel.name}:\n{summarized}")
+                # Send the summary result to the summary channel
+                await summary_channel.send(f"Summary for today in {channel.name}:\n{summarized}")
         except discord.errors.Forbidden:
             logging.error(f"Skipping channel {channel.name} due to insufficient permissions.")
             continue
-    if not found_messages:
-        logging.info(f"No messages found for the specified time range.")
-    return found_messages
 
 def summarize_text(text):
     openai.api_key = OPENAI_API_KEY
@@ -70,24 +78,6 @@ def summarize_text(text):
             return f"OpenAI API call failed: {e}"
     return ' '.join(summarized_chunks)
 
-async def summarize_channel_logs(guild, start_time, end_time):
-    found_messages = await fetch_logs(guild, start_time, end_time)
-    summary_channel = discord.utils.get(guild.text_channels, name=SUMMARY_CHANNEL_NAME)  # specify your summary channel name here
-    if summary_channel is None:
-        logging.error(f"Summary channel not found. Make sure '{SUMMARY_CHANNEL_NAME}' exists.")
-        return
-    for channel in guild.text_channels:
-        messages = found_messages.get(channel.id, [])
-        if not messages:
-            logging.info(f"No activity in channel {channel.name}. It was quiet today~")
-            continue
-        contents = ' '.join([message['content'] for message in messages])
-        summarized = summarize_text(contents)
-        logging.info(f"Summary for channel {channel.name}:\n{summarized}")
-        # Send the summary result to the summary channel
-        await summary_channel.send(f"Summary for today in {channel.name}:\n{summarized}")
-    return
-
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
@@ -100,7 +90,7 @@ async def summarize(ctx):
     logging.info(f"Processing request from {ctx.guild.name} ...")
     jst = pytz.timezone("Asia/Tokyo")
     start_time, end_time = get_start_and_end_times(jst)
-    await summarize_channel_logs(ctx.guild, start_time, end_time)
+    await fetch_and_summarize_channel_logs(ctx.guild, start_time, end_time)
     logging.info("Done processing request.")
 
 # botの起動
