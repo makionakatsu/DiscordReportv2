2c325b97fb3ac8be86e271c27c1892dacd408cb7
maki_ <ny.2151102@gmail.com>
2023-07-31 22:35:52 +0900

    エラーハンドリング追加

--- generate_channelid_json.py
+++ generate_channelid_json.py
@@ -7,6 +7,11 @@ from nextcord.ext import commands
 discord_token = os.getenv('DISCORD_TOKEN')
 guild_id = os.getenv('GUILD_ID')
 
+# 環境変数が設定されていることを確認
+if not discord_token or not guild_id:
+    print("Error: DISCORD_TOKEN and GUILD_ID environment variables must be set.")
+    exit(1)
+
 # Botのインスタンスを作成します
 bot = commands.Bot(command_prefix='!')
 
@@ -19,16 +24,18 @@ async def on_ready():
 
     # チャンネルのリストを作成
     channels = [
-        {"guild_id": str(guild.id), 
-         "channel_id": str(channel.id),
+        {"channel_id": str(channel.id),
          "channel_name": channel.name
         } 
         for channel in guild.channels
     ]
 
     # JSON形式で出力
-    with open('channels.json', 'w') as f:
-        json.dump({"skip_channels": channels}, f, indent=2)
+    try:
+        with open('channels.json', 'w') as f:
+            json.dump({"skip_channels": channels}, f, indent=2)
+    except Exception as e:
+        print(f"Error occurred while writing to JSON: {e}")
 
     # Botを終了
     await bot.close()
