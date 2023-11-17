610aee33b0c7b25d8eb9ac873ae65fb9db8519f6
maki_ <ny.2151102@gmail.com>
2023-11-17 12:58:05 +0900

    複数のチャンネルに対応

--- proposal_zatsudan_summary.py
+++ proposal_zatsudan_summary.py
@@ -6,14 +6,14 @@ import openai
 import os
 
 # 環境変数の設定
-proposal_summary_channel_id = os.getenv('PROPOSAL_SUMMARY_CHANNEL_ID')
+channel_ids = os.getenv('CHANNEL_IDS').split(',')  # 複数のチャンネルIDをリストとして取得
 bot_token = os.getenv('DISCORD_TOKEN')
 openai_api_key = os.getenv('OPENAI_API_KEY')
 emojis = os.getenv('EMOJIS', "\U0001F5E3\U0000FE0F,\U0001F9E0,\U0001F916").split(',')
 
 # 環境変数のチェック
-for var, name in [(proposal_summary_channel_id, 'PROPOSAL_SUMMARY_CHANNEL_ID'), (bot_token, 'DISCORD_TOKEN'), (openai_api_key, 'OPENAI_API_KEY')]:
-    if var is None:
+for var, name in [(channel_ids, 'CHANNEL_IDS'), (bot_token, 'DISCORD_TOKEN'), (openai_api_key, 'OPENAI_API_KEY')]:
+    if not var:
         print(f"Error: {name} is not set")
         exit(1)
 
@@ -64,9 +64,11 @@ def summarize_with_gpt(text):
         response_summary = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-16k",
             messages=[
-                {"role": "system", "content": "あなたはCHIPSくんという名前です。Discordの毎日のチャットログを確認し、日本語でトピックの包括的な要約を提供する役割を担うアシスタントです。"},
-                {"role": "user", "content": f"""以下のtextをもとに、各話題ごとに、(1)議題、(2)反論、(3)議論の総合的なまとめを出力してください。 
-                日本語で全部で200字程度で出力してください。text: {text}"""},
+                {"role": "system", "content": """あなたはCHIPSくんという名前です。Discordの毎日のチャットログを確認し、
+                日本語でトピックの包括的な要約を提供する役割を担うアシスタントです。
+                以下のtextをもとに、各話題ごとに、(1)議題、(2)反論、(3)議論のまとめを出力してください。
+                日本語で全部で200字程度で出力してください。"""},
+                {"role": "user", "content": {text}},
             ],
             max_tokens=600
         )
@@ -78,47 +80,48 @@ def summarize_with_gpt(text):
 
 # メイン処理
 if __name__ == "__main__":
-    filtered_messages = []
-    last_message_id = None
+    for channel_id in channel_ids:
+        filtered_messages = []
+        last_message_id = None
 
-    # メッセージを取得してフィルタリング
-    while True:
-        messages = fetch_messages(proposal_summary_channel_id, bot_token, last_message_id)
-        if not messages:
-            break
-        for msg in messages:
-            msg_time = datetime.fromisoformat(msg['timestamp'].replace('Z', '+00:00')).astimezone(jst)
-            if start_time <= msg_time <= end_time:
-                if 'reactions' in msg:
-                    if any(emoji in reaction['emoji']['name'] for reaction in msg['reactions'] for emoji in emojis):
-                        filtered_messages.append({
-                            "Message_Date": msg['timestamp'],
-                            "Author_Name": msg['author']['username'],
-                            "Content": msg['content'],
-                            "Reaction_Type": [reaction['emoji']['name'] for reaction in msg['reactions']]
-                        })
-        if messages:
-            try:
-                last_message_id = messages[-1]['id']
-            except KeyError:
-                print("Could not find 'id' key in the last message.")
+        # メッセージを取得してフィルタリング
+        while True:
+            messages = fetch_messages(channel_id, bot_token, last_message_id)
+            if not messages:
                 break
+            for msg in messages:
+                msg_time = datetime.fromisoformat(msg['timestamp'].replace('Z', '+00:00')).astimezone(jst)
+                if start_time <= msg_time <= end_time:
+                    if 'reactions' in msg:
+                        if any(emoji in reaction['emoji']['name'] for reaction in msg['reactions'] for emoji in emojis):
+                            filtered_messages.append({
+                                "Message_Date": msg['timestamp'],
+                                "Author_Name": msg['author']['username'],
+                                "Content": msg['content'],
+                                "Reaction_Type": [reaction['emoji']['name'] for reaction in msg['reactions']]
+                            })
+            if messages:
+                try:
+                    last_message_id = messages[-1]['id']
+                except KeyError:
+                    print("Could not find 'id' key in the last message.")
+                    break
 
-    # メッセージ日時でソート
-    filtered_messages.sort(key=lambda x: datetime.fromisoformat(x['Message_Date'].replace('Z', '+00:00')))
+        # メッセージ日時でソート
+        filtered_messages.sort(key=lambda x: datetime.fromisoformat(x['Message_Date'].replace('Z', '+00:00')))
 
-    # 抽出されたメッセージをJSONに出力
-    with open('filtered_messages.json', 'w', encoding='utf-8') as jsonfile:
-        json.dump(filtered_messages, jsonfile, ensure_ascii=False, indent=4)
+        # 抽出されたメッセージをJSONに出力
+        with open('filtered_messages.json', 'w', encoding='utf-8') as jsonfile:
+            json.dump(filtered_messages, jsonfile, ensure_ascii=False, indent=4)
 
-    # メッセージ内容を連結して要約を作成
-    text_to_summarize = " ".join([msg['Content'] for msg in filtered_messages])
-    summary = summarize_with_gpt(text_to_summarize)
+        #  メッセージ内容を連結して要約を作成
+        text_to_summarize = " ".join([msg['Content'] for msg in filtered_messages])
+        summary = summarize_with_gpt(text_to_summarize)
 
-    # 要約とその他の情報をDiscordに投稿
-    if summary:
-        full_message = f"CHIPSくんだよ！議論のサマリーを開始するよー:\n{summary}\n\n議論まとめはこれでおしまい！\n🗣️,🤖,🧠のいずれかのリアクションがあるメッセージを対象に議論のまとめを行っているよ。\n議論に関係あるメッセージにはリアクションをつけてね！"
-        print(full_message)
-        post_summary_to_discord(proposal_summary_channel_id, full_message, bot_token)
-    else:
-        print("要約を生成できませんでした。")
+        # 要約とその他の情報をDiscordに投稿
+        if summary:
+            full_message = f"CHIPSくんだよ！議論のサマリーを開始するよー:\n{summary}\n\n議論まとめはこれでおしまい！\n🗣️,🤖,🧠のいずれかのリアクションがあるメッセージを対象に議論のまとめを行っているよ。\n議論に関係あるメッセージにはリアクションをつけてね！"
+            print(full_message)
+            post_summary_to_discord(channel_id, full_message, bot_token)
+        else:
+            print("要約を生成できませんでした。")
